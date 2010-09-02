3c634f15791a4072265abf034446ef8828439698
Ignatyev Vjacheslav <ivv.private@gmail.com>
2010-09-02 09:36:40 +0400

    добавил google docs

--- shortcuts.js
+++ shortcuts.js
@@ -6,6 +6,11 @@ interactive("open-gmail", "Go to gmail", "follow-new-buffer",
             $browser_object = "http://gmail.com/");
 define_key(content_buffer_normal_keymap, "f2", "open-gmail");
 
+interactive("open-gdocs", "Go to google docs", "follow-new-buffer",
+            $browser_object = "http://docs.google.com/");
+define_key(content_buffer_normal_keymap, "f3", "open-gdocs");
+
 interactive("open-transmission", "Go to transmission", "follow-new-buffer",
             $browser_object = "http://localhost:9091/transmission/web");
-define_key(content_buffer_normal_keymap, "f3", "open-transmission");
+define_key(content_buffer_normal_keymap, "f9", "open-transmission");
+
