f5e9d04c7415f06697cbca74eb96ab94db6467da
Ignatyev Vjacheslav <ivv.private@gmail.com>
2010-09-15 11:35:59 +0400

    +google calendar

--- shortcuts.js
+++ shortcuts.js
@@ -10,6 +10,10 @@ interactive("open-gdocs", "Go to google docs", "follow-new-buffer",
             $browser_object = "http://docs.google.com/");
 define_key(content_buffer_normal_keymap, "f3", "open-gdocs");
 
+interactive("open-gcal", "Go to google calendar", "follow-new-buffer",
+            $browser_object = "http://calendar.google.com/");
+define_key(content_buffer_normal_keymap, "f4", "open-gcal");
+
 interactive("open-transmission", "Go to transmission", "follow-new-buffer",
             $browser_object = "http://localhost:9091/transmission/web");
 define_key(content_buffer_normal_keymap, "f9", "open-transmission");
